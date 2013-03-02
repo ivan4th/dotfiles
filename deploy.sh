fb6933651df002015bda260ff0716f4e1e82ed87
Ivan Shvedunov <ivan4th@gmail.com>
2013-03-03 03:49:23 +0400

    It's better this way.

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd "$(dirname $0)"
+src_dir="$PWD"
+dotkeep="$HOME/.dotkeep"
+linkfile () {
+    src="$1"
+    dotfile=".$(basename "$src")"
+    dotname="$HOME/$dotfile"
+    echo -n "$src --> $dotname ... " 1>&2
+    if [ -f "$dotname" -a ! -h "$dotname" ]; then
+      mkdir -p "$dotkeep"
+      backup="$HOME/.dotkeep/$dotfile"
+      echo -n "[backup $backup] " 1>&2
+      rm -f "$backup"
+      cp -a "$dotname" "$backup"
+    fi
+    ln -fs "$src" "$dotname"
+    echo "done" 1>&2
+}
+
+common_dir="$src_dir/common"
+machine_dir="$src_dir/machines/$(hostname)"
+if [ ! -d "$machine_dir" ]; then
+    echo "no definition for machine $(hostname)" 1>& 2
+    exit 1
+fi
+
+for filename in "$common_dir"/* "$machine_dir"/*; do
+    linkfile "$filename"
+done
