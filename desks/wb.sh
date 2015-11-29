31cc5018bd1594ae1c79d1e610490390e3685d5e
Ivan Shvedunov <ivan4th@gmail.com>
2015-11-29 03:52:58 +0300

    wb: temporary chroot stuff.
    To be replaced with devenv.

--- desks/wb.sh
+++ desks/wb.sh
@@ -1,2 +1,7 @@
-settitle wbdev
+if [ "$HOSTTYPE" != "arm" ]; then
+    # don't change title after chroot
+    settitle wbdev
+fi
 cd ~/work/contactless
+alias ch="settitle wbch; /usr/bin/proot -R /home/wb-root -q qemu-arm -b /home/ivan4th:/home/ivan4th /bin/bash"
+alias chr="settitle wbchr; sudo /usr/bin/proot -R /home/wb-root -q qemu-arm -b /home/ivan4th:/home/ivan4th /bin/bash"
--- desks/wbch.sh
+++
@@ -1,8 +0,0 @@
-settitle wbch
-if [ ! -f /home/wb-root/proc/uptime ]; then
-    echo exit | sudo /home/wb-root/chroot_this.sh
-fi
-if [ ! -d /home/wb-root/home/ivan4th/work ]; then
-    sudo mount --bind /home/ivan4th /home/wb-root/home/ivan4th
-fi
-sudo chroot /home/wb-root/ su - ivan4th
