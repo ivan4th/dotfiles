fa73bb49c4fee59c1176916cc602f9f031d51239
Ivan Shvedunov <ivan4th@gmail.com>
2016-07-11 19:45:05 +0300

    Add I4_GO_VERSION var (incomplete).

--- common/bash_06_go.sh
+++ common/bash_06_go.sh
@@ -1,5 +1,6 @@
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
-gvm use go1.6.1 >& /dev/null
+export I4_GO_VERSION=go1.6.2
+# gvm use "$I4_GO_VERSION" >& /dev/null
 
 export GOPATH="$HOME/wbdev/go:$GOPATH"
 export PATH="$PATH:$HOME/wbdev/go/bin"
