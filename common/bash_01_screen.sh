f2041256839750d6d82aaf2f115d2d931f312b28
Ivan Shvedunov <ivan4th@gmail.com>
2016-07-11 19:43:05 +0300

    Move settitle func to bash_01_screen.sh.

--- common/bash_01_screen.sh
+++ common/bash_01_screen.sh
@@ -4,3 +4,18 @@ mkdir -p ~/.cache/screen
 # serial consoles
 alias tu0='screen /dev/ttyUSB0 cs8,115200,ignbrk,-brkint,-icrnl,-imaxbel,-opost,-onlcr,-isig,-icanon,-iexten,-echo,-echoe,-echok,-echoctl,-echoke,noflsh,-ixon,-crtscts'
 alias tu1='screen /dev/ttyUSB1 cs8,115200,ignbrk,-brkint,-icrnl,-imaxbel,-opost,-onlcr,-isig,-icanon,-iexten,-echo,-echoe,-echok,-echoctl,-echoke,noflsh,-ixon,-crtscts'
+
+# http://code-and-hacks.peculier.com/articles/setting-terminal-title-in-gnu-screen/
+function settitle () {
+    if [[ "$TERM" != screen* ]]; then
+       return
+    fi
+    if [ -n "$STY" ] ; then
+        # We are in a screen session
+        # echo "Setting screen titles to $@"
+        printf "\033k%s\033\\" "$@"
+        # screen -X eval "at \\# title $@" "shelltitle $@"
+    else
+        printf "\033]0;%s\007" "$@"
+    fi
+}
--- common/bash_02_ssh.sh
+++ common/bash_02_ssh.sh
@@ -1,18 +1,3 @@
-# http://code-and-hacks.peculier.com/articles/setting-terminal-title-in-gnu-screen/
-function settitle () {
-    if [ "$TERM" != "screen" ]; then
-       return
-    fi
-    if [ -n "$STY" ] ; then
-        # We are in a screen session
-        # echo "Setting screen titles to $@"
-        printf "\033k%s\033\\" "$@"
-        # screen -X eval "at \\# title $@" "shelltitle $@"
-    else
-        printf "\033]0;%s\007" "$@"
-    fi
-}
-
 # rebuild ssh config
 ussh () {
     (echo "# GENERATED FILE!!!!! DO NOT EDIT!!!!!"; cat ~/.ssh/config.d/*) >~/.ssh/config
