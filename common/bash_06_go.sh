eb0a73924c84d1221dc5328dccb5d806ea8e45fb
Ivan Shvedunov <ishvedunov@mirantis.com>
2016-09-14 13:04:36 +0300

    Support k8s development

--- common/bash_06_go.sh
+++ common/bash_06_go.sh
@@ -2,5 +2,10 @@
 export I4_GO_VERSION=go1.6.2
 # gvm use "$I4_GO_VERSION" >& /dev/null
 
-export GOPATH="$HOME/wbdev/go:$GOPATH"
-export PATH="$PATH:$HOME/wbdev/go/bin"
+# export GOPATH="$HOME/wbdev/go:$GOPATH"
+# export PATH="$PATH:$HOME/wbdev/go/bin"
+if [[ "$I4SYSTYPE" == macosx ]]; then
+    export PATH=/usr/local/go/bin:$PATH
+fi
+
+export PATH="$HOME/go-tools/bin:$PATH"
--- desks/k.sh
+++ desks/k.sh
@@ -1,9 +1,38 @@
 settitle kube
-gvm use "$I4_GO_VERSION"
 export KPATH=$HOME/work/kubernetes
 export GOPATH=$KPATH
 export PATH=$PATH:$HOME/go-tools/bin
 export KUBERNETES_SRC_DIR=$KPATH/src/k8s.io/kubernetes
 cd $KUBERNETES_SRC_DIR
 export KUBERNETES_PROVIDER=local
-alias kc=$KUBERNETES_SRC_DIR/cluster/kubectl.sh
+alias kc="$KUBERNETES_SRC_DIR/cluster/kubectl.sh"
+
+function ssrc {
+    (
+        cd "$KUBERNETES_SRC_DIR"
+        if [ ! -f /tmp/lastSync ]; then
+            # TBD: push current branch
+            # TBD: delete test_tmp before that
+            # TBD: sync stashes (and remove --exclude .git/refs/stash)
+            git push -f vd:work/kubernetes/src/k8s.io/kubernetes test:test_tmp
+            rsync -avz \
+                  --filter=':- .gitignore' \
+                  --exclude .git/objects --exclude .git/refs/stash \
+                  --delete \
+                  "$HOME"/work/kubernetes/src/k8s.io/kubernetes/ \
+                  vd:work/kubernetes/src/k8s.io/kubernetes/
+        elif [ -z "$(find . -cnewer /tmp/lastSync -type f |head -1)" ]; then
+            echo "Everthing is up to date" 1>&2
+            return 0
+        else
+            # FIXME: empty dirs are not synced by this
+            # TBD: handle file removal
+            comm -12 <(find . -cnewer /tmp/lastSync -type f|sed 's@^\./@@'|sort) \
+                 <((git status --porcelain|sed 's/^.[^ ]* *//'; \
+                    ssh vd "cd ~/work/kubernetes/src/k8s.io/kubernetes && git status --porcelain|sed 's/^.[^ ]* *//'")|sort) |
+                xargs tar -cz |
+                ssh vd tar -C /home/vagrant/work/kubernetes/src/k8s.io/kubernetes -xvz
+        fi
+        touch /tmp/lastSync
+    )
+}
