ebfe45a61f645f164fe4194e6065c62799cbd90a
Ivan Shvedunov <ivan4th@gmail.com>
2020-06-22 11:31:12 +0300

    Add salt desk

--- desks/salt.sh
+++ desks/salt.sh
@@ -1,2 +1,9 @@
 settitle salt
 cd ~/work/lea/salt-state
+
+export SALT_CONFIG_DIR=~/work/lea/salt-state/testing
+
+function sm {
+  rm -rf $SALT_CONFIG_DIR/var/cache/salt
+  salt-master --log-level=debug --log-file=/tmp/salt.log
+}
